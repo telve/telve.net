cd429d619d670de42ce900141f2d04e78d57c06f
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2018-04-28 06:24:39 +0300

    Wrap the image downloaders with try-catch statements

--- application/controllers/Utilities.php
+++ application/controllers/Utilities.php
@@ -17,14 +17,18 @@
         public function download_all_topic_images()
         {
             if ($this->input->is_cli_request()) {
-                $topics_array = $this->link_model->retrieve_all_topics();
-                foreach ($topics_array as &$topic) {
-                    echo $topic['topic'];
-                    echo "<br>\n";
-                    echo $topic['header_image'];
-                    echo "<br>\n\n";
-                    $ext = pathinfo(parse_url($topic['header_image'], PHP_URL_PATH), PATHINFO_EXTENSION);
-                    copy($topic['header_image'], 'assets/img/topics/'.$topic['topic'].'.'.$ext);
+                try {
+                    $topics_array = $this->link_model->retrieve_all_topics();
+                    foreach ($topics_array as &$topic) {
+                        echo $topic['topic'];
+                        echo "<br>\n";
+                        echo $topic['header_image'];
+                        echo "<br>\n\n";
+                        $ext = pathinfo(parse_url($topic['header_image'], PHP_URL_PATH), PATHINFO_EXTENSION);
+                        copy($topic['header_image'], 'assets/img/topics/'.$topic['topic'].'.'.$ext);
+                    }
+                } catch (Exception $e) {
+                    // echo 'Caught exception: ',  $e->getMessage(), "\n";
                 }
             }
         }
@@ -32,25 +36,29 @@
         public function download_all_link_images()
         {
             if ($this->input->is_cli_request()) {
-                $links_array = $this->link_model->retrieve_all_links();
-                foreach ($links_array as &$link) {
-                    echo $link['title'];
-                    echo "<br>\n";
-                    echo $link['picurl'];
-                    echo "<br>\n\n";
-                    $ext = pathinfo(parse_url($link['picurl'], PHP_URL_PATH), PATHINFO_EXTENSION);
-                    $target_path = 'assets/img/link_thumbnails/'.$this->hashids->encode($link['id']).'.'.$ext;
-                    copy($link['picurl'], $target_path);
+                try {
+                    $links_array = $this->link_model->retrieve_all_links();
+                    foreach ($links_array as &$link) {
+                        echo $link['title'];
+                        echo "<br>\n";
+                        echo $link['picurl'];
+                        echo "<br>\n\n";
+                        $ext = pathinfo(parse_url($link['picurl'], PHP_URL_PATH), PATHINFO_EXTENSION);
+                        $target_path = 'assets/img/link_thumbnails/'.$this->hashids->encode($link['id']).'.'.$ext;
+                        copy($link['picurl'], $target_path);
 
-                    $config['image_library'] = 'gd2';
-                    $config['source_image'] = $target_path;
-                    $config['create_thumb'] = true;
-                    $config['maintain_ratio'] = true;
-                    $config['width']         = 248;
+                        $config['image_library'] = 'gd2';
+                        $config['source_image'] = $target_path;
+                        $config['create_thumb'] = true;
+                        $config['maintain_ratio'] = true;
+                        $config['width']         = 248;
 
-                    $this->image_lib->initialize($config);
-                    $this->image_lib->resize();
-                    $this->image_lib->clear();
+                        $this->image_lib->initialize($config);
+                        $this->image_lib->resize();
+                        $this->image_lib->clear();
+                    }
+                } catch (Exception $e) {
+                    // echo 'Caught exception: ',  $e->getMessage(), "\n";
                 }
             }
         }
