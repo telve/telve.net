7e0cb99cd70705d7b31cfdd0c69d55ff96b3f2bc
Mehmet Mert Yıldıran <mehmetmertyildiran@gmail.com>
2018-06-30 04:29:33 +0300

    Rename the link endpoint as links

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -18,12 +18,12 @@ class Api extends REST_Controller {
 
         // Configure limits on our controller methods
         // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
-        $this->methods['link_get']['limit'] = 500; // 500 requests per hour per user/key
+        $this->methods['links_get']['limit'] = 500; // 500 requests per hour per user/key
         $this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
         $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
     }
 
-    public function link_get()
+    public function links_get()
     {
         $offset = $this->get('offset');
         $offset = (int) $offset;
