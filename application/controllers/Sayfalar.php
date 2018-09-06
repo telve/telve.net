352bfe9be9ceba861b3f5fb7cab985e5a0219029
Evg <budo@narod.ru>
2018-09-06 21:14:18 +0300

    Add description
    Search engines require a unique value for each page.

--- application/controllers/Sayfalar.php
+++ application/controllers/Sayfalar.php
@@ -11,6 +11,7 @@
         public function blog()
         {
             $this->data['title'] = 'Blog';
+            $this->data['description'] = 'Blog...';
             $this->load->view('templates/header', $this->data);
             $this->load->view('sayfalar/blog', $this->data);
             $this->load->view('templates/side');
@@ -20,6 +21,7 @@
         public function reklam_ver()
         {
             $this->data['title'] = 'Reklam ver';
+            $this->data['description'] = 'Reklam ver...';
             $this->load->view('templates/header', $this->data);
             $this->load->view('sayfalar/reklam_ver', $this->data);
             $this->load->view('templates/side');
@@ -29,6 +31,7 @@
         public function kariyer()
         {
             $this->data['title'] = 'Kariyer';
+            $this->data['description'] = 'Kariyer...';
             $this->load->view('templates/header', $this->data);
             $this->load->view('sayfalar/kariyer', $this->data);
             $this->load->view('templates/side');
@@ -38,6 +41,7 @@
         public function kullanici_sozlesmesi()
         {
             $this->data['title'] = 'Kullanıcı Sözleşmesi';
+            $this->data['description'] = 'Kullanıcı Sözleşmesi...';
             $this->load->view('templates/header', $this->data);
             $this->load->view('sayfalar/kullanici_sozlesmesi', $this->data);
             $this->load->view('templates/side');
@@ -47,6 +51,7 @@
         public function gizlilik_politikasi()
         {
             $this->data['title'] = 'Gizlilik Politikası';
+            $this->data['description'] = 'Gizlilik Politikası...';
             $this->load->view('templates/header', $this->data);
             $this->load->view('sayfalar/gizlilik_politikasi', $this->data);
             $this->load->view('templates/side');
@@ -56,6 +61,7 @@
         public function site_kurallari()
         {
             $this->data['title'] = 'Site Kuralları';
+            $this->data['description'] = 'Site Kuralları...';
             $this->load->view('templates/header', $this->data);
             $this->load->view('sayfalar/site_kurallari', $this->data);
             $this->load->view('templates/side');
@@ -65,6 +71,7 @@
         public function sss()
         {
             $this->data['title'] = 'Sıkça Sorulan Sorular';
+            $this->data['description'] = 'Sıkça Sorulan Sorular...';
             $this->load->view('templates/header', $this->data);
             $this->load->view('sayfalar/sss', $this->data);
             $this->load->view('templates/side');
@@ -74,6 +81,7 @@
         public function bize_ulasin()
         {
             $this->data['title'] = 'Bize ulaşın';
+            $this->data['description'] = 'Bize ulaşın...';
             $this->load->view('templates/header', $this->data);
             $this->load->view('sayfalar/bize_ulasin', $this->data);
             $this->load->view('templates/side');
@@ -83,6 +91,7 @@
         public function iphone()
         {
             $this->data['title'] = 'Telve Apple Store Uygulaması';
+            $this->data['description'] = 'Telve Apple Store Uygulaması...';
             $this->load->view('templates/header', $this->data);
             $this->load->view('sayfalar/iphone', $this->data);
             $this->load->view('templates/side');
@@ -92,6 +101,7 @@
         public function android()
         {
             $this->data['title'] = 'Telve Google Play Store Uygulaması';
+            $this->data['description'] = 'Telve Google Play Store Uygulaması...';
             $this->load->view('templates/header', $this->data);
             $this->load->view('sayfalar/android', $this->data);
             $this->load->view('templates/side');
@@ -101,6 +111,7 @@
         public function altin()
         {
             $this->data['title'] = 'telvealtın';
+            $this->data['description'] = 'telvealtın...';
             $this->load->view('templates/header', $this->data);
             $this->load->view('sayfalar/altin', $this->data);
             $this->load->view('templates/side');
@@ -110,6 +121,7 @@
         public function sponsor()
         {
             $this->data['title'] = 'telvesponsor';
+            $this->data['description'] = 'telvesponsor...';
             $this->load->view('templates/header', $this->data);
             $this->load->view('sayfalar/sponsor', $this->data);
             $this->load->view('templates/side');
@@ -119,6 +131,7 @@
         public function markdown_rehberi()
         {
             $this->data['title'] = 'Markdown Rehberi';
+            $this->data['description'] = 'Markdown Rehberi...';
             $this->load->view('templates/header', $this->data);
             $this->load->view('sayfalar/markdown_rehberi', $this->data);
             $this->load->view('templates/side');
